de6672d60d8c0577cbf160c08461139dcafec623
AurelianTactics <jamesdilorenzo@gmail.com>
2018-07-27 08:56:44 -0400

    Add files via upload

--- script.lua
+++ script.lua
@@ -1,3 +1,13 @@
+function clip(v, min, max)
+    if v < min then
+        return min
+    elseif v > max then
+        return max
+    else
+        return v
+    end
+end
+
 previous_bubbles = 0
 function correct_bubbles()
   if data.bubbles > previous_bubbles then
@@ -17,23 +27,37 @@ function done_check()
 end
 
 previous_score = 0
+death_score = -1000
+clip_min = -1000
+clip_max = 1000
+
 function correct_score ()
-  local current_score = 0
-  local hundreds = (data.score_hyaku % 16)*100
-  local thousands = (math.floor(data.score_hyaku/16))*1000
-  local ten_thousands = (data.score_man % 16)*10000
-  local hundred_thousands = (math.floor(data.score_man/16))*100000
-  current_score = data.score_jyuu * 10 + hundreds + thousands + ten_thousands + hundred_thousands
-  if current_score > previous_score then
-    local delta = current_score - previous_score
-    previous_score = current_score
-    return delta
-  elseif previous_score - current_score > 500000 then
-    --not sure what happens when you get over 1,000,000. if it resets then this catches it
-	delta = 1000000 - previous_score + current_score
-	previous_score = current_score
-	return delta
+  local delta = 0
+  if done_check() then
+    --penalty for dieing
+    delta = death_score
   else
-    return 0
+    local current_score = 0
+    local tens = (data.score_jyuu / 16) * 10
+    local hundreds = (data.score_hyaku % 16)*100
+    local thousands = (math.floor(data.score_hyaku/16))*1000
+    local ten_thousands = (data.score_man % 16)*10000
+    local hundred_thousands = (math.floor(data.score_man/16))*100000
+    current_score = tens + hundreds + thousands + ten_thousands + hundred_thousands
+
+    if current_score > previous_score then
+      delta = current_score - previous_score
+      previous_score = current_score
+      if delta == 50 then
+        delta = 0 --no reward for wall bounces
+      end
+    elseif previous_score - current_score > 500000 then
+      --not sure what happens when you get over 1,000,000. if it resets then this catches it
+      delta = 1000000 - previous_score + current_score
+      previous_score = current_score
+    end
   end
+
+  delta = clip(delta, clip_min, clip_max)
+  return delta
 end
