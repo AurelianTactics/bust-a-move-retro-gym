29492e23c8544a3f600579ad7944c7eec50bc44c
AurelianTactics <jamesdilorenzo@gmail.com>
2018-08-07 01:13:01 -0400

    Add files via upload

--- contra3/script.lua
+++ contra3/script.lua
@@ -32,17 +32,17 @@ end
 
 
 prev_score = 0
-death_score = -1.0 * math.log(10000)
-level_clear_bonus = math.log(100000)
+death_score = -.1 ---1.0 * math.log(10000)
+level_clear_bonus = 100 --math.log(100000)
 clip_min = -15
-clip_max = 15
+clip_max = 100
 prev_lives_for_score = data.lives
-death_frames_penalty = 10
+death_frames_penalty = 30
 death_frames_iter = 0
 
 function correct_score()
   local delta = 0
-
+  --print('score is ' .. data.score)
   if done_check() then
     if level ~= data.level then
       delta = delta + level_clear_bonus
@@ -50,20 +50,23 @@ function correct_score()
       --penalty for game over
       delta = delta + death_score
     end
+    return delta
   elseif data.lives < prev_lives_for_score then
     prev_lives_for_score = data.lives
     delta = delta + death_score
     death_frames_iter = death_frames_penalty
-  else
-    if data.score > prev_score then
-      delta = delta + math.log(1+data.score-prev_score)
-      prev_score = data.score
-    elseif prev_score < data.score then
-      --not sure if score ever resets, never really figured out the rom address to score calculation
-      prev_score = data.score
-    end
-    delta = scrollable_adjustment(delta)
   end
+
+  --print('finding new score 1 ' .. data.score .. '  ' .. prev_score)
+  if data.score > prev_score then
+    delta = delta + math.log(1+data.score-prev_score)
+    --print('finding new score 2')
+  end
+  --not sure if score ever resets, never really figured out the rom address to score calculation
+  prev_score = data.score
+
+  delta = scrollable_adjustment(delta)
+
   
   --penalty for death lasts for death_frames_penalty frames
   if death_frames_iter > 0 then
@@ -78,9 +81,9 @@ end
 x_scroll_pos = 127 --player x position that enables level to scroll
 x_scroll_constant = 1 --value that tells screen is horizontal scrollable
 y_scroll_constant = 3 --value that tells screen is vert scrollable
-scroll_reward = math.log(10)
+scroll_reward = 0.05 --math.log(10)
 scroll_multiplier = 0.0 --penalty for if level is scrollable and player is not progressing in level
-existence_penalty = -1.0 * math.log(1.01) --life is suffering
+existence_penalty = -1.0 * math.log(1.1) --life is suffering
 prev_scroll_value = 0
 prev_vert_scroll_value = 0
 
@@ -92,11 +95,11 @@ function scrollable_adjustment(r)
       r = r + scroll_reward
     else
       --user position >= 127 to scroll
-      local distance_penalty = 0.
+      --local distance_penalty = 0.
       --if data.x1 < x_scroll_pos then
       --  distance_penalty = -1 * math.log(1 + math.abs(x_scroll_pos-data.x1)/127.0) 
       --end
-      r = r * scroll_multiplier --+ distance_penalty
+      r = clip(r,clip_min,scroll_reward*.5)--r -- scroll_multiplier --+ existence_penalty --+ distance_penalty
     end
   elseif data.is_scrollable == y_scroll_constant then
     if data.vert_scroll_value ~= prev_vert_scroll_value then
@@ -104,9 +107,10 @@ function scrollable_adjustment(r)
       prev_vert_scroll_value = data.vert_scroll_value
       r = r + scroll_reward
     else
-      r = r * scroll_multiplier
+      r = clip(r,clip_min,scroll_reward*.5) --r * scroll_multiplier --+ existence_penalty
     end
   else
+    --print("in scrollable adjustment " .. r)
     r = r --+ existence_penalty
   end
   return r
